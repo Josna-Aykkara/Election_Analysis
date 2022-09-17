de8aafbe4c15e27ba55d2da26807f3d0ad43d78b
unknown <josna.aykkara@gmail.com>
2022-09-17 11:35:37 -0400

    Adding the election analysis

---
+++ Python_practice2.py
@@ -0,0 +1,7 @@
+counties_dict =[{"Arapahoe", "Denver", "Jefferson"}]
+counties_dict["Arapahoe"] = 422829
+counties_dict["Jefferson"] = 432438
+counties_dict {'Arapahoe':422829, 'Denver':463353, 'Jefferson':432438}
+print(counties_dict)
+
+print (len(counties_dict))
